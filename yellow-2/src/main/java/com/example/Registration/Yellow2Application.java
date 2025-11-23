c082c5506bc09c65a303a39e1e4dbd87ef8ad379
Prajakta Kokane <kokaneprajakta4@gmail.com>
2025-11-23 18:48:57 +0530

    Add files via upload

---
+++ yellow-2/src/main/java/com/example/Registration/Yellow2Application.java
@@ -0,0 +1,14 @@
+package com.example.Registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yellow2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yellow2Application.class, args);
+		System.out.println("hello...");
+	}
+
+}
---
+++ yellow-2/src/test/java/com/example/Registration/Yellow2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yellow2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
